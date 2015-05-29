56d06ef538e16e7b6594efef5c514686f0e1a94b
nshaverdashvili <ninashaverdashvili@gmail.com>
2015-05-30 03:00:08 +0400

    New Project

---
+++ SMR/Core/DB/DBCon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace Core.DB
+{
+    public class DBCon
+    {
+        public static UMDataContext GetUMDataContext()
+        {
+            return new UMDataContext(ConfigurationManager.ConnectionStrings["DBConString"].ConnectionString);
+        }
+        public static GeneralDataContext GetGeneralDataContext()
+        {
+            return new GeneralDataContext(ConfigurationManager.ConnectionStrings["DBConString"].ConnectionString);
+        }
+    }
+}
---
+++ SMR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SMR.Global" Language="C#" %>
